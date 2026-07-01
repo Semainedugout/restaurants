4fb46e0bc4bf832f5e3fb777554afcd54b1754c1
Semainedugout <secretariat@gout.ch>
2026-07-01 17:03:11 +0200

    Update airtable.js

--- netlify/functions/airtable.js
+++ netlify/functions/airtable.js
@@ -1,18 +1,29 @@
 exports.handler = async function(event) {
   const token = process.env.AIRTABLE_TOKEN;
   const { table, formula } = event.queryStringParameters;
-  
-  const url = `https://api.airtable.com/v0/appspqjIM0kHmnXQr/${encodeURIComponent(table)}?pageSize=100${formula ? '&filterByFormula=' + encodeURIComponent(formula) : ''}`;
-  
-  const response = await fetch(url, {
-    headers: { Authorization: `Bearer ${token}` }
-  });
-  
-  const data = await response.json();
-  
+
+  let allRecords = [];
+  let offset = null;
+
+  do {
+    let url = `https://api.airtable.com/v0/appspqjIM0kHmnXQr/${encodeURIComponent(table)}?pageSize=100`;
+    if (formula) url += '&filterByFormula=' + encodeURIComponent(formula);
+    if (offset) url += '&offset=' + encodeURIComponent(offset);
+
+    const response = await fetch(url, {
+      headers: { Authorization: `Bearer ${token}` }
+    });
+
+    const data = await response.json();
+    
+    if (data.records) allRecords = allRecords.concat(data.records);
+    offset = data.offset || null;
+
+  } while (offset);
+
   return {
     statusCode: 200,
     headers: { 'Access-Control-Allow-Origin': '*' },
-    body: JSON.stringify(data)
+    body: JSON.stringify({ records: allRecords })
   };
 };
